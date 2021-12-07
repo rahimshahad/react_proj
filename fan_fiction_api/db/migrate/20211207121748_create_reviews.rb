6ba5652489e990c210aa35943d2ea1484cbc85f2
rahimshahad <rahim.shahad2@gmail.com>
2021-12-07 07:58:04 -0500

    add object relationships to models and migrations

--- fan_fiction_api/db/migrate/20211207121748_create_reviews.rb
+++ fan_fiction_api/db/migrate/20211207121748_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.string :content
-
+      t.belongs_to :movie
       t.timestamps
     end
   end
