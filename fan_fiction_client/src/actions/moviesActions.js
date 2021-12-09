05d167a92fa824f744ff30501ea67f7d994b9e75
rahimshahad <rahim.shahad2@gmail.com>
2021-12-09 17:43:20 -0500

    add fetch request for all movies

---
+++ fan_fiction_client/src/actions/moviesActions.js
@@ -0,0 +1,9 @@
+// an action to fetch all movies from api
+
+export const fetchMovies = () =>{
+    return (dispatch) => {
+        fetch('localhost:3000/movies')
+        .then(resp => resp.json)
+        .then(movies => console.table(movies))
+    }
+}
