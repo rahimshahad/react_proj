3fa895edfbf5fd2782e743cdfdae5058d55c6751
rahimshahad <rahim.shahad2@gmail.com>
2021-12-09 13:20:07 -0500

    createstore and render application to DOM

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension';
+// create store accepts a reducer, dev tools
+
+
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={(store)}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
