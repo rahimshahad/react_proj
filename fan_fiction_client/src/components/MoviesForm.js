98379e321b4c0fea97290d5e77567a6fa0d97b9c
rahimshahad <rahim.shahad2@gmail.com>
2021-12-10 11:05:03 -0500

    add and define handleChange event listener to input fields

--- fan_fiction_client/src/components/MoviesForm.js
+++ fan_fiction_client/src/components/MoviesForm.js
@@ -7,21 +7,29 @@ export default class MoviesForm extends Component {
         setting: '',
         genre: ''
     }
+
+    handleChange = (e) =>{
+        const {name, value} = e.target
+
+        this.setState({
+            [name]: value
+        })
+    }
     render() {
         return (
             <div>
                <form>
                    <label>Title:</label>
-                   <input type='text' value={this.state.title} name='title'/>
+                   <input type='text' value={this.state.title} name='title' onChange={this.handleChange}/>
                    <br/>
                    <label>Plot:</label>
-                   <textarea value={this.state.plot} name='plot'/>
+                   <textarea value={this.state.plot} name='plot' onChange={this.handleChange}/>
                    <br/>
                    <label>Setting:</label>
-                   <input type='text' value={this.state.setting} name='setting'/>
+                   <input type='text' value={this.state.setting} name='setting' onChange={this.handleChange}/>
                    <br/>
                    <label>Genre:</label>
-                   <input type='text' value={this.state.genre} name='genre'/>
+                   <input type='text' value={this.state.genre} name='genre' onChange={this.handleChange}/>
                    <br/>
                    <input type='submit' value='Create Movie' />
                </form>
