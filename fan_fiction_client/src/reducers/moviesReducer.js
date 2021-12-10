1ebe967cab82dd3c9dbdadff1a61e4485030ce5c
rahimshahad <rahim.shahad2@gmail.com>
2021-12-10 11:13:58 -0500

    add case for 'ADD_MOVIE' to movies reducer

--- fan_fiction_client/src/reducers/moviesReducer.js
+++ fan_fiction_client/src/reducers/moviesReducer.js
@@ -2,6 +2,8 @@ export const moviesReducer = (state =[], action) => {
     switch(action.type){
         case 'FETCH_MOVIES':
             return action.payload  
+        case 'ADD_MOVIE':
+                return [...state, action.payload]
             default:
                     return state
     }
