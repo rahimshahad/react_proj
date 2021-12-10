1799d5c56a4f29ddbdcac4219eeaab3c3830d7a1
rahimshahad <rahim.shahad2@gmail.com>
2021-12-09 20:28:01 -0500

    create form for creating movies

--- fan_fiction_client/src/components/MoviesForm.js
+++ fan_fiction_client/src/components/MoviesForm.js
@@ -10,7 +10,21 @@ export default class MoviesForm extends Component {
     render() {
         return (
             <div>
-               
+               <form>
+                   <label>Title:</label>
+                   <input type='text' value={this.state.title} name='title'/>
+                   <br/>
+                   <label>Plot:</label>
+                   <input type='text' value={this.state.plot} name='plot'/>
+                   <br/>
+                   <label>Setting:</label>
+                   <input type='text' value={this.state.setting} name='setting'/>
+                   <br/>
+                   <label>Genre:</label>
+                   <input type='text' value={this.state.genre} name='genre'/>
+                   <br/>
+                   <input type='submit' value='Create Movie' />>
+               </form>
             </div>
         )
     }
