772cf82c7b6b1f6089f4ca6dfac606a37028104c
rahimshahad <rahim.shahad2@gmail.com>
2021-12-10 11:44:32 -0500

    import necessary components into router.js

--- fan_fiction_client/src/components/Router.js
+++ fan_fiction_client/src/components/Router.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Home from '../components/Home'
+import MoviesContainer from './MoviesContainer'
+import About from '../components/About'
 
 export default function Router() {
     return (
