c5ec871cd819ab49511d3e3beda77f807a2108e2
rahimshahad <rahim.shahad2@gmail.com>
2021-12-11 15:16:12 -0500

    create form for editing movie

--- fan_fiction_client/src/components/EditMovieForm.js
+++ fan_fiction_client/src/components/EditMovieForm.js
@@ -5,7 +5,21 @@ import {connect} from 'react-redux'
     render() {
         return (
             <div>
-                
+                 <Form onSubmit={this.handleSubmit} >
+                   <label>Title:</label>
+                   <FormInput type='text' value={this.state.title} name='title' onChange={this.handleChange}/>
+                   <br/>
+                   <label>Plot:</label>
+                   <textarea value={this.state.plot} name='plot' onChange={this.handleChange}/>
+                   <br/>
+                   <label>Setting:</label>
+                   <FormInput type='text' value={this.state.setting} name='setting' onChange={this.handleChange}/>
+                   <br/>
+                   <label>Genre:</label>
+                   <FormInput type='text' value={this.state.genre} name='genre' onChange={this.handleChange}/>
+                   <br/>
+                   <FormInput type='submit' value='Update Movie' />
+               </Form>
             </div>
         )
     }
