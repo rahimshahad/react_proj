e1e9e9db9b5953292c60c1f047fb499b6ac31d81
rahimshahad <rahim.shahad2@gmail.com>
2021-12-11 09:54:01 -0500

    finish login

--- fan_fiction_api/app/controllers/application_controller.rb
+++ fan_fiction_api/app/controllers/application_controller.rb
@@ -7,4 +7,20 @@ class ApplicationController < ActionController::API
     def logged_in?
         !!session[:user_id]
     end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def authorized_user?
+        @user == current_user
+    end
+
+    def logout!
+        session.clear
+    end
+
+    def set_user
+        @user = User.find_by(id: session[:user_id])
+    end
 end
