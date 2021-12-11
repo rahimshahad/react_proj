d5e288ad75f4f6e409d647a05a27d3a2b39a0fb9
rahimshahad <rahim.shahad2@gmail.com>
2021-12-10 23:33:40 -0500

    create styled form

--- fan_fiction_client/src/components/MoviesForm.js
+++ fan_fiction_client/src/components/MoviesForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { addMovie } from '../actions/moviesActions'
+import { Form, FormInput } from '../styled-components/Form.style'
 
  class MoviesForm extends Component {
     state ={
@@ -25,21 +26,21 @@ import { addMovie } from '../actions/moviesActions'
     render() {
         return (
             <div>
-               <form onSubmit={this.handleSubmit} >
+               <Form onSubmit={this.handleSubmit} >
                    <label>Title:</label>
-                   <input type='text' value={this.state.title} name='title' onChange={this.handleChange}/>
+                   <FormInput type='text' value={this.state.title} name='title' onChange={this.handleChange}/>
                    <br/>
                    <label>Plot:</label>
                    <textarea value={this.state.plot} name='plot' onChange={this.handleChange}/>
                    <br/>
                    <label>Setting:</label>
-                   <input type='text' value={this.state.setting} name='setting' onChange={this.handleChange}/>
+                   <FormInput type='text' value={this.state.setting} name='setting' onChange={this.handleChange}/>
                    <br/>
                    <label>Genre:</label>
-                   <input type='text' value={this.state.genre} name='genre' onChange={this.handleChange}/>
+                   <FormInput type='text' value={this.state.genre} name='genre' onChange={this.handleChange}/>
                    <br/>
-                   <input type='submit' value='Create Movie' />
-               </form>
+                   <FormInput type='submit' value='Create Movie' />
+               </Form>
             </div>
         )
     }
