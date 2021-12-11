82c4a7290b30546505cdd3e9898b1c5d3c472637
rahimshahad <rahim.shahad2@gmail.com>
2021-12-11 14:54:43 -0500

    create editMovie action to handle fetch requests for editing movies

--- fan_fiction_client/src/actions/moviesActions.js
+++ fan_fiction_client/src/actions/moviesActions.js
@@ -1,30 +1,42 @@
 // an action to fetch all movies from api
 
-export const fetchMovies = () =>{
-    return (dispatch) => {
-        fetch('http://localhost:3000/movies')
-        .then(resp => resp.json())
-        .then(movies => dispatch({type: 'FETCH_MOVIES', payload: movies}))
-    }
-}
+export const fetchMovies = () => {
+  return (dispatch) => {
+    fetch("http://localhost:3000/movies")
+      .then((resp) => resp.json())
+      .then((movies) => dispatch({ type: "FETCH_MOVIES", payload: movies }));
+  };
+};
 
-export const addMovie = (movie) =>{
-    return (dispatch) =>{ 
-    fetch('http://localhost:3000/movies',{
-        method:'POST',
-        body: JSON.stringify(movie),
-        headers: { 'Content-Type':'application/json'}
+export const addMovie = (movie) => {
+  return (dispatch) => {
+    fetch("http://localhost:3000/movies", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: { "Content-Type": "application/json" },
     })
-        .then(resp => resp.json())
-        .then(movie => dispatch({type: 'ADD_MOVIE', payload: movie}))
-}
-}
+      .then((resp) => resp.json())
+      .then((movie) => dispatch({ type: "ADD_MOVIE", payload: movie }));
+  };
+};
 
 export const deleteMovie = (movieId) => {
-    return (dispatch) =>{
-        fetch(`http://localhost:3000/movies/${movieId}`, {
-            method: 'DELETE',
-        })
-        dispatch({type: 'DELETE_MOVIE', payload: movieId})
-    }
-}
+  return (dispatch) => {
+    fetch(`http://localhost:3000/movies/${movieId}`, {
+      method: "DELETE",
+    });
+    dispatch({ type: "DELETE_MOVIE", payload: movieId });
+  };
+};
+
+export const editMovie = (movie) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/movies/${movie.id}`, {
+      method: "PATCH",
+      body: JSON.stringify(movie),
+      headers: { "content-type": "application/json" },
+    })
+      .then((resp) => resp.json())
+      .then((movie) => dispatch({ type: "EDIT_MOVIE", payload: movie }));
+  };
+};
--- fan_fiction_client/src/reducers/moviesReducer.js
+++ fan_fiction_client/src/reducers/moviesReducer.js
@@ -1,18 +1,21 @@
-const initialState = []
+const initialState = [];
 export const moviesReducer = (state = initialState, action) => {
-    switch(action.type){
-        case 'FETCH_MOVIES':
-            return [...state, ...action.payload]
-        case 'ADD_MOVIE':
-            return [...state, ...action.payload]
-        case 'DELETE_MOVIE':
-            state = state.filter(movie => movie.id !== action.payload)
-            return [...state]  
-        case 'EDIT_MOVIE':
-            [...state.map(movie => movie.id === action.payload.id ? action.payload : movie)]
+  switch (action.type) {
+    case "FETCH_MOVIES":
+      return [...state, ...action.payload];
+    case "ADD_MOVIE":
+      return [...state, ...action.payload];
+    case "DELETE_MOVIE":
+      state = state.filter((movie) => movie.id !== action.payload);
+      return [...state];
+    case "EDIT_MOVIE":
+      [
+        ...state.map((movie) =>
+          movie.id === action.payload.id ? action.payload : movie
+        ),
+      ];
 
-        default:
-            return state
-
-    }
-}
+    default:
+      return state;
+  }
+};
