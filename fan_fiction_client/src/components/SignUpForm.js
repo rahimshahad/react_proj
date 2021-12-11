75ea547894232ea7847704212a20e55c3d297719
rahimshahad <rahim.shahad2@gmail.com>
2021-12-11 06:15:46 -0500

    create signup form

--- fan_fiction_client/src/components/SignUpForm.js
+++ fan_fiction_client/src/components/SignUpForm.js
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
 import {Form, FormInput} from "../styled-components/Form.style"
 
-class SignUp extends Component{
+export default class SignUp extends Component{
 
     state= {
         username: "",
         password: ""
     }
-}
+
+    render(){
+        return(
+            <div>
+                <Form >
+                    <label>Username:</label>
+                    <FormInput name='username' value={this.state.username} />
+                    <br/> 
+                    <label>Password:</label>
+                    <FormInput name='password' type='password' />
+                    <br/>
+                    <FormInput type='submit' value='Sign Up' />
+
+                </Form>
+            </div>
+        )
+    }
+};
