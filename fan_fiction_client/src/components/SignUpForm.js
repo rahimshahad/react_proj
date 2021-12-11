36612c8c9ac61b5eccb1c70901c0da887f919069
rahimshahad <rahim.shahad2@gmail.com>
2021-12-11 06:05:32 -0500

    set initial state for sign up form

---
+++ fan_fiction_client/src/components/SignUpForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {Form, FormInput} from "../styled-components/Form.style"
+
+class SignUp extends Component{
+
+    state= {
+        username: "",
+        password: ""
+    }
+}
